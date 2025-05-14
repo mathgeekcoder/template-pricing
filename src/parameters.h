b02c7b019c1653118d744adad81fa2b4b14ada5e
Luke Marshall <52978038+mathgeekcoder@users.noreply.github.com>
2025-05-13 18:13:32 -0700

    Changed timeout to be 4 hours

--- src/parameters.h
+++ src/parameters.h
@@ -3,7 +3,7 @@
 
 struct Parameters {
 	bool show_output = true;
-	double timeout = 86400;  // 1 day
+	double timeout = 14400;  // 4 hours
 	double gap = 0.00001;
 	int nodes = 1; // std::numeric_limits<int>::max(); // max no limit, 1 root node, ...
 	int random_seed = 0;
