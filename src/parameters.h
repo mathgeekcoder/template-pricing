2ac58229d14ce343664a724db5458f17d66548f8
Luke Marshall <52978038+mathgeekcoder@users.noreply.github.com>
2025-08-08 18:44:54 -0700

    Change params to ints

--- src/parameters.h
+++ src/parameters.h
@@ -8,8 +8,8 @@ struct Parameters {
 	double age_limit = 1;
 
 	int random_seed = 0;
-	uint8_t nodes = 1; // 0 for farkas, 1 for root node (> 0 for branching, support has been removed)
-	uint8_t num_threads = 1;
+	int nodes = 1; // 0 for farkas, 1 for root node (> 0 for branching, support has been removed)
+	int num_threads = 1;
 	bool show_output = true;
 	std::string column_retention = "low"; // Options: low, med, high
 };
