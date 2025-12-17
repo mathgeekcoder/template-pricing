1d238b5c9bfe1393e46d50e3eb378ec34074d317
Luke Marshall <52978038+mathgeekcoder@users.noreply.github.com>
2025-12-17 13:10:11 -0800

    added `save_original` to GapInstance to convert the new instances to old schema

--- src/gap/gap_instance.h
+++ src/gap/gap_instance.h
@@ -22,6 +22,33 @@ struct GapInstance {
     std::vector<std::vector<int>> demands;
     std::vector<std::vector<double>> profit;
 
+    GapInstance(std::string file = "") {
+        machines = jobs = 0;  // initialize to 0 in case of loading error
+        name = std::filesystem::path(file).filename().string();
+
+        // check file extension (JSON or original)
+        std::string ext = std::filesystem::path(file).extension().string();
+
+        if (ext == ".json") {
+            load_json(read_file(file));
+        }
+        else if (ext == ".gz") {
+            load_json(read_gz_file(file));
+        }
+        else {
+            load_original(file);
+        }
+    }
+
+    void load_json(std::string json_content) {
+        json j = json::parse(json_content);
+        machines = j["nAgents"];
+        jobs = j["nTasks"];
+        capacity = j["capacity"].get<std::vector<int>>();
+        profit = j["cost"].get<std::vector<std::vector<double>>>();
+        demands = j["usage"].get<std::vector<std::vector<int>>>();
+    }
+
     void load_original(std::string file) {
         std::ifstream in(file);
         if (in.is_open()) {
@@ -42,32 +69,24 @@ struct GapInstance {
         }
 	}
 
-    void load_json(std::string json_content) {
-        json j = json::parse(json_content);
-        machines = j["nAgents"];
-        jobs = j["nTasks"];
-        capacity = j["capacity"].get<std::vector<int>>();
-        profit = j["cost"].get<std::vector<std::vector<double>>>();
-        demands = j["usage"].get<std::vector<std::vector<int>>>();
-	}
-
-    GapInstance(std::string file = "") {
-        machines = jobs = 0;  // initialize to 0 in case of loading error
-        name = std::filesystem::path(file).filename().string();
-
-        // check file extension (JSON or original)
-        std::string ext = std::filesystem::path(file).extension().string();
-
-        if (ext == ".json") {
-            load_json(read_file(file));
+    void save_original(const std::string& filename) {
+        std::ofstream out(filename);
+        out << machines << " " << jobs << "\n";
+        for (int m = 0; m < machines; m++) {
+            for (int j = 0; j < jobs; j++)
+                out << profit[m][j] << " ";
+            out << "\n";
         }
-        else if (ext == ".gz") {
-            load_json(read_gz_file(file));
-		}
-        else {
-            load_original(file);
-		}
-    }
+        for (int m = 0; m < machines; m++) {
+            for (int j = 0; j < jobs; j++)
+                out << demands[m][j] << " ";
+            out << "\n";
+        }
+        for (int m = 0; m < machines; m++)
+            out << capacity[m] << " ";
+        out << "\n";
+        out.close();
+	}
 };
 
 
